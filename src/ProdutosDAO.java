961ebdb0a21321c04a34c936d6ba57084d6d11ec
Ricardo191005 <ric19oliveira@gmail.com>
2025-10-03 10:30:24 -0300

    Adicionado método venderProduto()

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -3,17 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException; 
+import javax.swing.JOptionPane;
 import java.util.ArrayList;
-
 
 public class ProdutosDAO {
     
@@ -23,20 +18,30 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
         return listagem;
     }
     
-    
-    
-        
+    public boolean venderProduto(int idProduto) {
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+        try {
+            conn = new conectaDAO().connectDB(); 
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, idProduto);
+            int linhasAfetadas = prep.executeUpdate();
+            return linhasAfetadas > 0;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + e.getMessage());
+            return false;
+        } finally {
+            try {
+                if (prep != null) prep.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+            }
+        }
+    }
 }
 
